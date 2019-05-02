29348274c85dc5c752cfd4359d187c6e368b1e05
admin <bionischetech@gmail.com>
2019-05-02 17:02:09 +0530

    //Ganesh

---
+++ src/main/java/com/bionische/lms/BionischeLabMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.bionische.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BionischeLabMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BionischeLabMgmtApplication.class, args);
+	}
+
+}
